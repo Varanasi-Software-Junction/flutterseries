d5b6c98b7ecc9d678bb6c4405e23a50a919cb1b3
Sanvi Verma <120442954+TheSanvi@users.noreply.github.com>
2023-06-27 11:42:49 +0530

    Update jsontoobject.dart

--- jsontoobject.dart
+++ jsontoobject.dart
@@ -1,5 +1,19 @@
-{
-"name": "Popat",
-"age": 30,
-"roll_no": 24
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
+
+void main() async {
+  String completeurl =
+      "https://gist.githubusercontent.com/TheSanvi/5cf07f06a291742690b9e9d62bb8ebde/raw/e071f925ed46bc5a2fa33ce1f3ca065e4c967336/data.json";
+
+  final url = Uri.https(
+      "gist.githubusercontent.com",
+      "/TheSanvi/5cf07f06a291742690b9e9d62bb8ebde/raw/e071f925ed46bc5a2fa33ce1f3ca065e4c967336/data.json",
+      {});
+
+  final response = await http.get(url);
+
+  print(response);
+  print(response.statusCode);
+  final jsonResponse = response.body;
+  print(jsonResponse);
 }
